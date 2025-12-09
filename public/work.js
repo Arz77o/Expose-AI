8ac8c6867cad50c903a3a12943a8405f28cddd6a
Arbouz Abderrahmane <abdouarbouz56@gmail.com>
2025-12-09 09:39:45 +0100

    Delete public directory

--- public/work.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuBtn = document.getElementById("menuBtn");
-    const mobileMenu = document.getElementById("mobileMenu");
-
-    menuBtn.addEventListener("click", () => {
-        mobileMenu.classList.toggle("hidden");
-    });
-});
